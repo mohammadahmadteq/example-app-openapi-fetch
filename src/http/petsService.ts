2f391be4acddd5e1bc0f77ce13c0fea9eac3801c
root <root@DESKTOP-FUUHGOR.localdomain>
2024-06-28 17:30:18 +0500

    frontend

--- src/http/petsService.ts
+++ src/http/petsService.ts
@@ -1,3 +1,4 @@
+/*
 import {IPet} from "../types/petTypes.tsx";
 
 export const getPets = async () => {
@@ -10,4 +11,27 @@ export const getPets = async () => {
         console.log("Cannot Get");
         return [];
     }
+};
+*/
+
+import createClient from "openapi-fetch";
+import type { paths } from "../../src/types/swaggerTypes.ts";
+import { IPet } from "../types/petTypes.tsx";
+
+const client = createClient<paths>({ baseUrl: "http://localhost:3020" });
+
+export const getPets = async (): Promise<IPet[]> => {
+    try {
+        const { data, error } = await client.GET("/petsapp/pet");
+
+    if (error) {
+        console.error("Error fetching pets:", error);
+        throw new Error("Cannot Get");
+    }
+
+    return data as IPet[];
+} catch (error) {
+    console.error("Cannot Get");
+    return [];
+}
 };
